5f66025353658901a07fab9173ab4a0398f40f8f
Bjoern Meyer <bjoern@textcontrol.com>
2024-07-11 13:31:59 +0200

    Add project files.

---
+++ tx_packing/Program.cs
@@ -0,0 +1,18 @@
+﻿using TXTextControl.DocumentServer;
+
+using (TXTextControl.ServerTextControl tx = new TXTextControl.ServerTextControl())
+{
+	tx.Create();
+	tx.Load("packing_slip.tx", TXTextControl.StreamType.InternalUnicodeFormat);
+
+	MailMerge mailMerge = new MailMerge()
+	{
+		TextComponent = tx
+	};
+
+	string jsonData = System.IO.File.ReadAllText("data.json");
+
+	mailMerge.MergeJsonData(jsonData);
+
+	tx.Save("output.pdf", TXTextControl.StreamType.AdobePDF);
+}
